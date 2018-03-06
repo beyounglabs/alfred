ecc28e404827b75be525c1bde42358d14e19904f
Piero Giusti <pierophp@gmail.com>
2018-03-06 14:52:25 -0300

    Brain JS SDK

---
+++ src/env/load.env.ts
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+import { BrainParameter } from 'brain-js-sdk';
+
+export async function loadEnv(subscribe: boolean) {
+  let dotenvPath = '.env';
+
+  if (process.env.NODE_ENV === 'testing') {
+    dotenvPath = '.env.testing';
+  }
+
+  const { parsed } = dotenv.config({ path: dotenvPath });
+
+  process.env = JSON.parse(JSON.stringify(process.env));
+
+  const brainRedisOpts = {
+    host: process.env.BRAIN_REDIS_HOST,
+    port: parseInt(process.env.BRAIN_REDIS_PORT || '6379', 10),
+  };
+
+  const brainParameter = new BrainParameter(
+    brainRedisOpts,
+    process.env.BRAIN_SERVICE || '',
+    process.env.BRAIN_PROFILE || '',
+  );
+
+  await brainParameter.updateEnv();
+
+  if (subscribe) {
+    brainParameter.subscribe().then();
+  } else {
+    brainParameter.closeConnection();
+  }
+}
