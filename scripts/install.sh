f32151201ce4045e0761ab0d4df5d2afd8372e33
Piero Giusti <pierophp@gmail.com>
2018-03-06 18:28:57 -0300

    Remove echo

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,6 @@ SCRIPTPATH=`dirname $SCRIPT`
 FOLDER_FULL_PATH=`realpath "$SCRIPTPATH/../../"`
 FOLDER=`basename "$FOLDER_FULL_PATH"`
 
-echo $SCRIPTPATH
-echo $FOLDER
-
 if [ $FOLDER == "node_modules" ]
 then
     yarn tsc && cp -r dist/* .
