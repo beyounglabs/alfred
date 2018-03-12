784fcd22b0e75ecb70adf195c5a710fe250bbd70
Piero Giusti <pierophp@gmail.com>
2018-03-12 11:35:43 -0300

    QueueGenerator

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { loadEnv } from './env/load.env';
 import { Logger } from './logger/logger';
 import { DatabaseProvider } from './providers/database.provider';
 import { RedisProvider } from './providers/redis.provider';
+import { QueueGenerator } from './queue/queue.generator';
 import { FixtureAbstract } from './tests/fixture.abstract';
 import { TestCase } from './tests/test.case';
 import { BaseRepository } from './typeorm/base.repository';
@@ -9,6 +10,7 @@ import { DefaultMetadata } from './typeorm/default.metadata';
 
 export {
   loadEnv,
+  QueueGenerator,
   BaseRepository,
   DefaultMetadata,
   DatabaseProvider,
