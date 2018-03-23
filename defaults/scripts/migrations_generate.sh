dbea1f3ce4ec4473eec69af8d7955b7d6c17c341
Piero Giusti <pierophp@gmail.com>
2018-03-23 15:28:56 -0300

    Fix migrations generate Alfred

--- defaults/scripts/migrations_generate.sh
+++ defaults/scripts/migrations_generate.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 cd "${SCRIPT_DIR}/../";
 
 yarn cli migrations:generate -n $1
-cp -R dist/database/migrations/*.ts database/migrations/
+mv dist/database/migrations/*.ts database/migrations/
  
