038a90ae6f31956db21ce630d6faa018b388f1bc
Piero Giusti <pierophp@gmail.com>
2018-05-30 18:16:42 -0300

    Fix mystique fileld interface

--- src/mystique/contracts/mystique.text.field.interface.ts
+++ src/mystique/contracts/mystique.text.field.interface.ts
@@ -2,4 +2,6 @@ import { MystiqueBaseFieldInterface } from './mystique.base.field.interface';
 
 export interface MystiqueTextFieldInterface extends MystiqueBaseFieldInterface {
   type: 'text';
+  multiline: boolean;
+  rowsMax: number;
 }
