4087249a1c3bc0d73d4d58368c884e07f8a1bd0d
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2018-06-14 15:45:29 -0300

    export request helper from index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 export { loadEnv } from './env/load.env';
+export { jsonRequestMiddleware } from './express/json.request.middleware';
 export { loadRoutes } from './express/load.routes';
 export { ResponseInterface } from './express/response.interface';
-export { jsonRequestMiddleware } from './express/json.request.middleware';
 export { ObjectConverter } from './helpers/object.converter';
-export { roundNumber } from './helpers/round.number';
 export { RemoveAccents } from './helpers/remove.accents';
+export { request } from './helpers/request';
+export { roundNumber } from './helpers/round.number';
 export { ErrorLogger } from './logger/error.logger';
 export { InfoLogger } from './logger/info.logger';
 export { WarnLogger } from './logger/warn.logger';
-export { DatabaseProvider } from './providers/database.provider';
-export { RedisProvider } from './providers/redis.provider';
-export { QueueGenerator } from './queue/queue.generator';
-export { FixtureAbstract } from './tests/fixture.abstract';
-export { TestCase } from './tests/test.case';
-export { BaseRepository } from './typeorm/base.repository';
-export { DefaultMetadata } from './typeorm/default.metadata';
-export { getCustomRepository } from './typeorm/get.custom.repository';
-export { QueryManager } from './typeorm/query.manager';
 export { AbstractUpserter } from './mystique/abstract.upserter';
 export {
   MystiqueActionInterface,
@@ -27,4 +19,13 @@ export {
 export {
   MystiqueResultHeaderInterface,
 } from './mystique/contracts/mystique.result.header.interface';
+export { DatabaseProvider } from './providers/database.provider';
+export { RedisProvider } from './providers/redis.provider';
+export { QueueGenerator } from './queue/queue.generator';
+export { FixtureAbstract } from './tests/fixture.abstract';
+export { TestCase } from './tests/test.case';
+export { BaseRepository } from './typeorm/base.repository';
+export { DefaultMetadata } from './typeorm/default.metadata';
+export { getCustomRepository } from './typeorm/get.custom.repository';
+export { QueryManager } from './typeorm/query.manager';
 export { Validator } from './validator/validator';
