8b82bf3d1b30f8d2859b6ba90ca62018af9efc1a
Matheus Moraes <matheusmmo.01@gmail.com>
2018-07-10 12:12:06 -0300

    Stream inside extra

--- src/providers/database.provider.ts
+++ src/providers/database.provider.ts
@@ -62,6 +62,11 @@ export class DatabaseProvider {
         ],
         extra: {
           connectionLimit: process.env.DB_POOL_CONNECTION_LIMIT || 10,
+          stream: (opts) => {
+            const socket = net.connect(opts.config.port, opts.config.host);
+            socket.setKeepAlive(true);
+            return socket;
+          },
         },
       };
 
