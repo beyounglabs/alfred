a461091cfe9e73d6160656aec19a48393fab40b2
Piero Giusti <pierophp@gmail.com>
2018-08-06 14:34:14 -0300

    Fix ts config

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,22 @@ NODEMODULES_PATH=`pwd`;
 
 NODEMODULES_DIR=`basename $(pwd)`;
 
+cd "${SCRIPT_DIR}/../../../";
+
+REPOSITORY_NAME=`basename $(pwd)`;
+
+echo 'REPOSITORY:';
+echo $REPOSITORY_NAME;
+
 cd $ORIGINAL_PATH;
+
+echo $PWD
+echo $NODEMODULES_PATH
+
+if [ $REPOSITORY_NAME == "optimus" ]
+then
+  sed -i "s@es2017@es5@g" "${NODEMODULES_PATH}/alfred/tsconfig.json"
+fi
 
 if [ $NODEMODULES_DIR == "node_modules" ]
 then
