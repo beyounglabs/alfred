6898f69462c280d9fd5d64dbe1a29474caae8fbf
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2018-08-22 11:10:30 -0300

    DEV-1298: Handle response errors on HTTP 200 responses

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -1,11 +1,13 @@
-import Axios, { AxiosPromise, AxiosRequestConfig } from 'axios';
+import Axios, { AxiosRequestConfig } from 'axios';
+import { coalesce } from './coalesce';
+import { RequestError } from './request.error';
 
-export function request(
+export async function request(
   requestUrl: string,
   params: { [x: string]: any },
   baseUrl?: string,
   config?: AxiosRequestConfig,
-): AxiosPromise<any> {
+): Promise<any> {
   let [method, url] = requestUrl.split(':');
 
   if (!url) {
@@ -23,5 +25,22 @@ export function request(
     ...config,
   };
 
-  return Axios(requestConfig);
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await Axios(requestConfig);
+      const status = coalesce(() => response.data.status, 200);
+      const message = coalesce(() => response.data.message, null);
+
+      if (status >= 400 && status < 600) {
+        const error = new RequestError(message || 'No error message provided');
+        error.response = response;
+
+        throw error;
+      }
+
+      resolve(response);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
