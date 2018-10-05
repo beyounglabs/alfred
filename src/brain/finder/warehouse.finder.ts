0251a44548ac6b97535db440ff7104d76a9e4d68
Piero Giusti <pierophp@gmail.com>
2018-10-05 20:55:38 +0800

    Warehouse finder

---
+++ src/brain/finder/warehouse.finder.ts
@@ -0,0 +1,41 @@
+import { orderBy } from 'lodash';
+import { promisify } from 'util';
+import { ObjectConverter } from '../../helpers/object.converter';
+import { RedisManager } from '../redis.manager';
+
+export class WarehouseFinder {
+  public async findAll(): Promise<any> {
+    const redisManager = new RedisManager();
+    const redisClient = await redisManager.getClient();
+
+    const getAsync = promisify(redisClient.get).bind(redisClient);
+    const keysAsync = promisify(redisClient.keys).bind(redisClient);
+
+    const keys = await keysAsync(`Warehouse:*`);
+    const items: any[] = [];
+    for (const key of keys) {
+      items.push(JSON.parse(await getAsync(key)));
+    }
+
+    return orderBy(
+      ObjectConverter.underscoreToCamelCase(items),
+      ['name', 'code'],
+      ['ASC', 'ASC'],
+    );
+  }
+
+  public async findOneByCode(code: string): Promise<any> {
+    const redisManager = new RedisManager();
+    const redisClient = await redisManager.getClient();
+
+    const getAsync = promisify(redisClient.get).bind(redisClient);
+    const key = `Warehouse:${code}`;
+
+    const result = await getAsync(key);
+    if (!result) {
+      return;
+    }
+
+    return ObjectConverter.underscoreToCamelCase(JSON.parse(result));
+  }
+}
