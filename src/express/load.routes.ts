daaa586853685d767c222d1f2281803facbf3446
Piero Giusti <pierophp@gmail.com>
2018-10-23 00:21:10 +0800

    JWT Helper

--- src/express/load.routes.ts
+++ src/express/load.routes.ts
@@ -2,6 +2,7 @@ import { Express, NextFunction, Request } from 'express';
 import * as trimRequest from 'trim-request';
 import { QueryManager } from '../typeorm/query.manager';
 import { ResponseInterface } from './response.interface';
+import { JwtHelper } from '../helpers/jwt.helpers';
 
 export async function loadRoutes(app: Express, routes: any[]) {
   for (const route of routes) {
@@ -23,7 +24,28 @@ export async function loadRoutes(app: Express, routes: any[]) {
         });
 
         response.locals.queryManager = new QueryManager();
-        response.locals.route = route;
+
+        if (route.protected) {
+          let authorization: any = request.headers['authorization'];
+          if (!authorization) {
+            throw new Error('Token Bearer not found');
+          }
+
+          authorization = authorization.split('Bearer ');
+
+          if (authorization.length === 1) {
+            throw new Error('Token Bearer not found');
+          }
+
+          const auth = await JwtHelper.verify(authorization.length[1]);
+
+          if (!auth) {
+            throw new Error('Token Bearer invalid');
+          }
+
+          response.locals.auth = auth;
+        }
+
         try {
           await route.action(request, response);
           next();
--- src/express/response.interface.ts
+++ src/express/response.interface.ts
@@ -4,6 +4,6 @@ import { QueryManager } from '../typeorm/query.manager';
 export interface ResponseInterface extends Response {
   locals: {
     queryManager: QueryManager;
-    route: any;
+    auth: any;
   };
 }
