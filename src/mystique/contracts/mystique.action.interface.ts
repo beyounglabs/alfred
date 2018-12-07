7bdb400ef5eb41865f3624b9e73ba12f3f401f32
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2018-12-07 14:11:42 -0200

    (Feature) Adicionar `url` como atributo de uma ação

--- src/mystique/contracts/mystique.action.interface.ts
+++ src/mystique/contracts/mystique.action.interface.ts
@@ -2,6 +2,7 @@ export interface MystiqueActionInterface {
   code?: string;
   icon?: string;
   label: string;
+  url?: string;
   props: { [prop: string]: any };
   component?: {
     path: string;
