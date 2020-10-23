b20fd7cb7586d00cf7a618d0c4ec31224f16f5d4
Piero Giusti <pierophp@gmail.com>
2020-10-23 17:49:00 -0300

    Fix num transformer

--- src/typeorm/transformers/number.transformer.ts
+++ src/typeorm/transformers/number.transformer.ts
@@ -2,10 +2,18 @@ import { ValueTransformer } from 'typeorm/decorator/options/ValueTransformer';
 
 export class NumberTransformer implements ValueTransformer {
   public from(value) {
+    if (value === null || value === undefined) {
+      return value;
+    }
+
     return Number(value);
   }
 
   public to(value) {
+    if (value === null || value === undefined) {
+      return value;
+    }
+
     return Number(value);
   }
 }
