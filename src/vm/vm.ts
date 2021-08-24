e36946b9cddf6ee40defc1316b8224bc8746a44e
Piero Giusti <pierophp@gmail.com>
2021-08-24 10:32:00 -0300

    Fix vm bug

--- src/vm/vm.ts
+++ src/vm/vm.ts
@@ -3,7 +3,12 @@ import { VariablesInterface } from './variables.interface';
 
 export class Vm {
   public async run(code: string, variables: VariablesInterface): Promise<any> {
-    const variablesContext = vm.createContext(variables);
+    const newVariables = {};
+    for (const code of Object.keys(variables)) {
+      newVariables[code] = variables[code].value;
+    }
+
+    const variablesContext = vm.createContext(newVariables);
 
     const codeToRun = `(() => {
       return (${code});
