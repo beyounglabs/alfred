8c49eda04f53bbba6a9e80d54aaad73f848eb627
Piero Giusti <pierophp@gmail.com>
2021-09-17 08:10:07 -0300

    Memcached

--- src/cache/cache.factory.ts
+++ src/cache/cache.factory.ts
@@ -1,16 +1,28 @@
-import { LocalCache } from './drivers/local.cache';
-import { CacheInterface } from './cache.interface';
-import { RedisCache } from './drivers/redis.cache';
 import { Apm } from '../apm/apm';
+import { CacheInterface } from './cache.interface';
+import { LocalCache } from './drivers/local.cache';
+import { MemcachedCache } from './drivers/memcached.cache';
+import { RedisCache } from './drivers/redis.cache';
 
 export class CacheFactory {
   public static get(instance?: string, apm?: Apm): CacheInterface {
     const instancePrefix = instance?.toUpperCase() || 'default';
+
+    const driver =
+      process.env[`CACHE_${instancePrefix}_DRIVER`] ??
+      process.env.CACHE_DRIVER ??
+      'redis';
+
     if (
-      process.env.REDIS_CACHE_HOST ||
-      process.env[`REDIS_CACHE_${instancePrefix}_HOST`]
+      driver === 'redis' &&
+      (process.env.REDIS_CACHE_HOST ||
+        process.env[`REDIS_CACHE_${instancePrefix}_HOST`])
     ) {
       return new RedisCache(instance, apm);
+    }
+
+    if (driver === 'memcached') {
+      return new MemcachedCache(instance, apm);
     }
 
     return new LocalCache();
