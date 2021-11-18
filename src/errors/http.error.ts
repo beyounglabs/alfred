47bddd278d2b2944a9247ffea34894153c45052d
augustocheme <augusto.cheme@gmail.com>
2021-11-18 15:59:46 -0300

    Feat(Errors): Adicionar tipagens de erro

---
+++ src/errors/http.error.ts
@@ -0,0 +1,8 @@
+class HttpError extends Error{
+  protected status: number;
+
+  constructor(status: number, error: string){
+    super(error);
+    this.status = status;
+  }
+}
---
+++ src/errors/missing.record.error.ts
@@ -0,0 +1,8 @@
+class MissingRecordError extends Error{
+  protected status: number;
+
+  constructor(error: string){
+    super(error);
+    this.status = 404;
+  }
+}
