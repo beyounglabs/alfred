755c1bb2946ffbe0690d8f5fcc4dd46c5cc71202
Piero Giusti <pierophp@gmail.com>
2022-02-09 12:35:17 -0300

    Fix alfred

--- defaults/docker/api/entrypoint.sh
+++ defaults/docker/api/entrypoint.sh
@@ -2,12 +2,13 @@
 
 DIR="/var/www/html/node_modules"
 
+cp -r /home/docker/.ssh/ /root/
+chown -R root:root /root/.ssh 
+chmod -R 700 /root/.ssh 
+
 if [ -d "$DIR" ]; then
     yarn start
 else
-    cp -r /home/docker/.ssh/ /root/
-    chown -R root:root /root/.ssh 
-    chmod -R 700 /root/.ssh 
     yarn install
     chown -R docker:docker $DIR
     yarn start
