374b4d8e7524f3822e75854fad0dcac3bbbc2fa9
Piero Giusti <pierophp@gmail.com>
2022-06-06 02:20:12 -0300

    Add alfred

--- src/logger/error.logger.ts
+++ src/logger/error.logger.ts
@@ -1,79 +1,89 @@
+import { LoggingWinston } from '@google-cloud/logging-winston';
 import * as winston from 'winston';
-import * as SlackHook from 'winston-slack-webhook-transport';
-import { LoggerInterface } from './contracts/logger.interface';
-import * as Transports from 'winston-transport';
+import {
+  LogDataInterface,
+  LoggerInterface,
+} from './contracts/logger.interface';
+import { hostname } from 'os';
+import { ErrorInterface } from './contracts/error.interface';
 
-let logger: winston.Logger | null = null;
+let loggers: { [index: string]: winston.Logger | null } = {};
 
 const EXPIRATION_TIME = 3600000;
 
 export class ErrorLogger implements LoggerInterface {
-  constructor() {
-    this.expireCache();
-  }
+  protected data: ErrorInterface;
 
-  protected expireCache() {
+  constructor(data: ErrorInterface) {
+    this.data = data;
+
+    if (!data.errorIndex) {
+      data.errorIndex = 'default';
+    }
+
     setTimeout(() => {
-      logger = null;
-
-      this.expireCache();
+      loggers[this.data.errorIndex!] = null;
     }, EXPIRATION_TIME);
   }
 
   public getLogger(): winston.Logger {
-    if (logger) {
-      return logger;
+    if (loggers[this.data.errorIndex!]) {
+      return loggers[this.data.errorIndex!]!;
     }
 
-    const transports: Transports[] = [];
+    if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+      process.env.GOOGLE_APPLICATION_CREDENTIALS =
+        '/var/www/html/gcp-credentials.json';
+    }
 
-    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+    const transports: any[] = [];
 
-    const defaultChannel: string =
-      process.env.NODE_ENV === 'production' ? 'logger' : 'logger-staging';
+    const index = [
+      this.data.errorIndex,
+      String(process.env.BUILD || '000').toLowerCase(),
+      // `toISOString` returns date and time separated by 'T',
+      // so we remove everything after the 'T'.
+      new Date().toISOString().replace(/T.+$/, ''),
+    ].join('-');
 
-    if (slackWebhookUrl) {
-
-      const slackTransport = new SlackHook({
-        level: 'error',
-        webhookUrl: slackWebhookUrl,
-        channel: process.env.SLACK_CHANNEL || defaultChannel,
-        iconEmoji: ':shit:',
-        username: 'Logger',
-        formatter: (info) => {
-          const { level, message, ...meta } = info;
-
-          return {
-            text: `*${process.env.BRAIN_SERVICE} - ${
-              process.env.BRAIN_PROFILE
-            } - ${
-              process.env.NODE_ENV
-            }*\n*Message*: ${message}. \n\n \`\`\`${JSON.stringify(meta, null, 2)}\`\`\``,
-          };
+    transports.push(
+      new LoggingWinston({
+        level: 'warn',
+        labels: {
+          index,
+          build: process.env.BUILD || '',
+          env: process.env.NODE_ENV || '',
+          service: process.env.BRAIN_SERVICE || '',
+          profile: process.env.BRAIN_PROFILE || '',
+          hostname: hostname(),
+          pid: process.pid.toString(),
         },
-      });
+      }),
+    );
 
-      transports.push(slackTransport);
-
-    }
-
-    logger = winston.createLogger({
+    const logger = winston.createLogger({
       transports,
       exitOnError: false,
     });
 
+    loggers[this.data.errorIndex!] = logger;
+
     return logger;
   }
 
-  public async log(data: any): Promise<void> {
+  public async log(data: LogDataInterface): Promise<void> {
     try {
-      const message = data['message'] ? data['message'] : 'log_default';
+      const logger: winston.Logger = this.getLogger();
+
       if (process.env.NODE_ENV === 'development') {
-        console.error(message, data);
-        return;
+        console.error(data);
       }
 
-      const logger: winston.Logger = this.getLogger();
+      const message = data['message'] ? data['message'] : 'log_default';
+
+      if (this.isStatic()) {
+        data.content = JSON.stringify(data.content, null, 2);
+      }
 
       logger.error(message, data);
     } catch (e) {
@@ -81,11 +91,15 @@ export class ErrorLogger implements LoggerInterface {
     }
   }
 
+  public isStatic() {
+    return this.data.errorIndex!.endsWith('-static');
+  }
+
   public async close(): Promise<any> {
-    const currentLogger: winston.Logger = this.getLogger();
+    const logger: winston.Logger = this.getLogger();
 
-    logger = null;
+    loggers[this.data.errorIndex!] = null;
 
-    return Promise.resolve(currentLogger.close());
+    return Promise.resolve(logger.close());
   }
 }
