43888a5c43afc81f449896a9a49a96eb1485ab3e
Piero Giusti <pierophp@gmail.com>
2022-08-02 16:58:08 -0300

    Fix Logger

--- src/logger-v2/logger.generator.ts
+++ src/logger-v2/logger.generator.ts
@@ -2,6 +2,9 @@ import { hostname } from 'os';
 import * as winston from 'winston';
 import { LoggingWinston } from '@google-cloud/logging-winston';
 import { LoggerLevelType } from './logger.level.type';
+import { stat } from 'fs/promises';
+
+let logger: any = undefined;
 
 export abstract class LoggerGenerator {
   public static async log(
@@ -9,39 +12,44 @@ export abstract class LoggerGenerator {
     message: string,
     metadata: any,
   ): Promise<void> {
-    if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
-      process.env.GOOGLE_APPLICATION_CREDENTIALS =
-        '/var/www/html/gcp-credentials.json';
-    }
+    if (!logger) {
+      const fileExists = await stat('/var/www/html/gcp-credentials.json')
+        .then(() => true)
+        .catch(() => false);
 
-    const transports: any[] = [];
+      if (fileExists && !process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+        process.env.GOOGLE_APPLICATION_CREDENTIALS =
+          '/var/www/html/gcp-credentials.json';
+      }
+      const transports: any[] = [];
 
-    const index = [
-      String(process.env.BUILD || '000').toLowerCase(),
-      new Date().toISOString().replace(/T.+$/, ''),
-    ].join('-');
+      const index = [
+        String(process.env.BUILD || '000').toLowerCase(),
+        new Date().toISOString().replace(/T.+$/, ''),
+      ].join('-');
 
-    transports.push(
-      new LoggingWinston({
-        level: process.env.DEBUG_LEVEL ?? 'warning',
+      transports.push(
+        new LoggingWinston({
+          level: process.env.LOGGING_LEVEL ?? 'warning',
+          levels: winston.config.syslog.levels,
+          labels: {
+            index,
+            build: process.env.BUILD || '',
+            env: process.env.NODE_ENV || '',
+            service: process.env.BRAIN_SERVICE || '',
+            profile: process.env.BRAIN_PROFILE || '',
+            hostname: hostname(),
+            pid: process.pid.toString(),
+          },
+        }),
+      );
+
+      logger = winston.createLogger({
+        transports,
+        exitOnError: false,
         levels: winston.config.syslog.levels,
-        labels: {
-          index,
-          build: process.env.BUILD || '',
-          env: process.env.NODE_ENV || '',
-          service: process.env.BRAIN_SERVICE || '',
-          profile: process.env.BRAIN_PROFILE || '',
-          hostname: hostname(),
-          pid: process.pid.toString(),
-        },
-      }),
-    );
-
-    const logger = winston.createLogger({
-      transports,
-      exitOnError: false,
-      levels: winston.config.syslog.levels,
-    });
+      });
+    }
 
     try {
       if (process.env.NODE_ENV === 'development') {
