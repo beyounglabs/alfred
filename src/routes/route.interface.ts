66eaadd90faa25059e6dfc68132ab55b66940a52
Piero Giusti <pierophp@gmail.com>
2023-06-21 17:44:06 -0300

    HTTP Methods

--- src/routes/route.interface.ts
+++ src/routes/route.interface.ts
@@ -1,5 +1,24 @@
+type _HTTPMethods =
+  | 'DELETE'
+  | 'GET'
+  | 'HEAD'
+  | 'PATCH'
+  | 'POST'
+  | 'PUT'
+  | 'OPTIONS'
+  | 'PROPFIND'
+  | 'PROPPATCH'
+  | 'MKCOL'
+  | 'COPY'
+  | 'MOVE'
+  | 'LOCK'
+  | 'UNLOCK'
+  | 'TRACE'
+  | 'SEARCH';
+
+export type HTTPMethods = Uppercase<_HTTPMethods> | Lowercase<_HTTPMethods>;
 export interface RouteInterface {
-  method: string;
+  method: HTTPMethods;
   path: string | RegExp;
   action: any;
   timeout?: number;
