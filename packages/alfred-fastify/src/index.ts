91fc3786003fd34b40997f159059c8f693bfc9bc
Piero Giusti <pierophp@gmail.com>
2023-07-21 16:26:17 -0300

    Deploy

---
+++ packages/alfred-fastify/src/index.ts
@@ -0,0 +1,5 @@
+export { loadRoutes } from './load.routes';
+export { loadSwagger } from './load.swagger';
+export { RequestContext } from './request.context';
+export { RouteInterface } from './route.interface';
+export { SwaggerFastifySchema } from './swagger.fastify.schema';
