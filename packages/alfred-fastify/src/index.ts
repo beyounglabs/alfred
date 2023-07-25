e1ce8e4cfdd915ff7b9e3b9367ad690f44c7f3b4
Piero Giusti <pierophp@gmail.com>
2023-07-25 16:39:10 -0300

    Export server

--- packages/alfred-fastify/src/index.ts
+++ packages/alfred-fastify/src/index.ts
@@ -1,5 +1,6 @@
 export { loadRoutes } from './load.routes';
 export { loadSwagger } from './load.swagger';
+export { startServer } from './start.server';
 export { RequestContext } from './request.context';
 export { RouteInterface } from './route.interface';
 export { SwaggerFastifySchema } from './swagger.fastify.schema';
