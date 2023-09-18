c818d9e47db40d06cdf99fa78b849e6f3d699359
Piero Giusti <pierophp@gmail.com>
2023-09-18 20:09:50 -0300

    Alfred

--- packages/alfred-typeorm/src/providers/database.provider.ts
+++ packages/alfred-typeorm/src/providers/database.provider.ts
@@ -54,10 +54,10 @@ export class DatabaseProvider {
         logger: 'file',
         cache: false,
         entities: [
-          `${__dirname}/../../../../${prefix}src/entities/*.entity{.ts,.js}`,
+          `${__dirname}/../../../../../${prefix}src/entities/*.entity{.ts,.js}`,
         ],
         migrations: [
-          `${__dirname}/../../../../${prefix}database/migrations/*.{.ts,.js}`,
+          `${__dirname}/../../../../../${prefix}database/migrations/*.{.ts,.js}`,
         ],
         extra: {
           connectionLimit: process.env.DB_POOL_CONNECTION_LIMIT || 10,
