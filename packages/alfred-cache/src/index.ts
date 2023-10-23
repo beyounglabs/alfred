124950ab966ceae1f2ae99bd5fa5a0fc9fc887c0
Piero Giusti <pierophp@gmail.com>
2023-10-23 16:40:40 -0300

    Alfred Cache

--- packages/alfred-cache/src/index.ts
+++ packages/alfred-cache/src/index.ts
@@ -6,3 +6,7 @@ export {
   createRedisConnection,
   RedisMode,
 } from './redis.manager';
+
+export { BrotliCompression } from './compression/brotli.compression';
+export { GzipCompression } from './compression/gzip.compression';
+export { NoCompression } from './compression/no.compression';
